9cb74bbfbdb61387e662371d8739efdd03efb776
stills <stills-ogburn@Mac.lan>
2025-11-11 19:14:32 -0500

    data.py done

--- src/data.py
+++ src/data.py
@@ -4,57 +4,99 @@ from transformers import BertTokenizer
 import torch
 import lightning as L
 
-class IMDBDataModule(L.LighteningDataModule):
+# Import config at the top
+import config 
+
+class IMDBDataModule(L.LightningDataModule):
   """Downloads and prepares the IMDB Dataset for model
   training and loads the training, validation, and tests sets"""
   def __init__(self, tokenizer: BertTokenizer, batch_size: int, max_length: int, root: str = ".data"):
-          super().__init__()
-          self.root = root # directory where the IMbD dataset will be saved
-          self.tokenizer = tokenizer # BertTokenizer
-          self.max_length = max_length # fixed uniform length for every review
-          self.batch_size = batch_size # size of the batch for training
+    super().__init__()
+    self.root = root # directory where the IMbD dataset will be saved
+    self.tokenizer = tokenizer # BertTokenizer
+    self.max_length = max_length # fixed uniform length for every review
+    self.batch_size = batch_size # size of the batch for training
+    
+    # collate_fn expects a function that translate our reviews and ratings into a format
+    # the model can understand and use for training
+    self.collate_fn = self.translate
 
   def prepare_data(self):
-     """Downloads the IMDB dataset the .data directory
-     Important: torchtext.dataset provides the dataset in 2 parts - train & test;
-     Pytorch won't assign IMBD calls to a vars within prepare_data() """
-     # torchtext downloads the data as 2 parts: train and test
-    
-     IMDB(root=self.data_dir, split="train") # 25k
-     IMDB(root=self.data_dir, split="test") # 25k
+    """Downloads the IMDB dataset the .data directory
+    Important: torchtext.dataset provides the dataset in 2 parts - train & test;
+    Pytorch won't assign IMBD calls to a vars within prepare_data() """
+    IMDB(root=self.root, split="train") # 25k
+    IMDB(root=self.root, split="test") # 25k
   
-  def setup(self):
+  def setup(self, stage: str = None):
     """Seteup the training, validation, and test sets
     Reqs: 70 (train) /15 (validation)/ 15 (test)"""
 
-    import config # holds the hyperparamters for each of src files
-    train_set = IMDB(root=self.data_dir, split="train") # torchtext downloads the data as 2 parts: train and test
-    test_set = IMDB(root=self.data_dir, split="test")
+    # get the train and test pre-splits from IMBD
+    train_set = IMDB(root=self.root, split="train") 
+    test_set = IMDB(root=self.root, split="test")
 
     # combine the sets
     IMBD_dataset = ConcatDataset([train_set, test_set])
 
-    # full dataset size needed for 70/15/15 calc
+    # the dataset's size; ned this to compute for 70/15/15
     size = len(IMBD_dataset)
-    train_set_size = size * config.TRAIN_SPLIT
-    test_set_size = size * config.TEST_SPLIT
-    val_set_size = size * config.VAL_SPLIT
+    train_set_size = int(size * config.TRAIN_SPLIT)
+    val_set_size = int(size * config.VAL_SPLIT)
+    
+    # Make test_set_size the remainder to ensure they add up perfectly
+    test_set_size = size - train_set_size - val_set_size
 
+    # The order of sizes in the list MUST match the order of variables
     self.train_dataset, self.val_dataset, self.test_dataset = random_split(
       IMBD_dataset, 
-      [train_set_size, test_set_size, val_set_size],
+      [train_set_size, val_set_size, test_set_size],
       generator = torch.Generator().manual_seed(config.RANDOM_SEED)
   )
-
+    
+  def translate(self, batch):
+    """Translates the batch of reviews and ratings into numerical tensors for the model"""
+    # The IMDB dataset returns (rating, review).
+    ratings, reviews = zip(*batch) # unzips list of (rating, review) tuples
+    
+    # tokenize the batch of reviews
+    encoding = self.tokenizer(
+          reviews, # chagng tuple of reviews into a list
+          padding='max_length', # padding to standardize review tensors to size 256
+          truncation=True, # cut the review tensor if it's above 256
+          max_length=self.max_length, # size 256 for padding
+          return_tensors='pt' # "pt" is for pytorch, return pytorch tensors
+    )
+    # ratings: 1 (neg) and 2 (pos). Subtract 1 so that neg -> 0 and pos -> 1
+    ratings_tensor = torch.tensor(
+       [rate - 1 for rate in ratings],
+       dtype = torch.long
+    )
+    # single dictionary for the model
+    encoding["ratings"] = ratings_tensor
+    return encoding
+  
   def train_dataloader(self):
-    """loads the data for training dataset"""
+    """Load em (training set, validation set, and test set) up!"""
     return DataLoader(
+        self.train_dataset,
+        batch_size=self.batch_size,
+        shuffle=True,
+        collate_fn=self.collate_fn # Use the assigned function
     )
+    
   def val_dataloader(self):
-    """loads validation dataset"""
-    return DataLoader(
-    )
+      return DataLoader(
+          self.val_dataset,
+          batch_size=self.batch_size,
+          shuffle=False,
+          collate_fn=self.collate_fn
+      )
+      
   def test_dataloader(self):
-    """loads test dataset"""
-    return DataLoader(
-    )
+      return DataLoader(
+          self.test_dataset,
+          batch_size=self.batch_size,
+          shuffle=False,
+          collate_fn=self.collate_fn
+      )
